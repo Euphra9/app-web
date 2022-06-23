3dd5c8c1e87296da7f833bcda38dfc92c6e95ef4
Luzolo Euphraim <euphraluzolo@gmail.com>
2022-06-23 20:39:02 +0200

    API student OK

--- src/core/router/routes.js
+++ src/core/router/routes.js
@@ -2,19 +2,19 @@ module.exports = app => {
     const students = require("../../Controller/StudentsController.js");
     var router = require("express").Router();
   
-     // Retrieve all Tutorials
+     // Retrieve all Students
   router.get("/", students.findAll);
 
-    // Create a new Tutorial
+    // Create a new Student
   router.post("/", students.create);
 
- // Retrieve a single Tutorial with id
+ // Retrieve a single Student with id
  router.get("/:id", students.findOne);
 
-    // Update a Tutorial with id
+    // Update a Student with id
     router.put("/:id", students.update);
 
- // Delete a Tutorial with id
+ // Delete a Student with id
  router.delete("/:id", students.delete);
     
     app.use('/api/students', router);
