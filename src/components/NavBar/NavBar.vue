4ad20b5c04e3dd3303d6536e3024a26d925ba9cd
Luzolo Euphraim <euphraluzolo@gmail.com>
2022-06-28 23:59:18 +0200

    tableaux OK

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -2,17 +2,34 @@
 
   <div class="menu">
 
-    <div class="profile">
-      <img src="" alt="Profile Image" class="profile-img">
-      <p class="profile-name">Utilisateur : [insert nom]</p>
-      <a href="" target="_blank" rel="noopener noreferrer" class="profile-link">Profile</a>
-    </div>
+
 
     <div class="menu-items">
-      <router-link to="/pret"><img alt="loan" src="../../assets/loan.png"/></router-link>
-    <router-link to="/materiels"><img alt="material" src="https://img.icons8.com/dotty/80/000000/computer.png"/></router-link>
-      <router-link to="/etudiants"> <img alt="students" src="../../assets/students.png"/></router-link>
-     <img alt="statistic" src="../../assets/statistic.png"/>
+
+      <div>
+             <router-link to="/"><img alt="loan" src="../../assets/logo.png"/></router-link>
+             <p> Accueil</p>
+           </div>
+
+
+     <div>
+       <router-link to="/pret"><img alt="loan" src="https://img.icons8.com/ios/100/000000/change.png"/></router-link>
+      <p> Prêts</p>
+     </div>
+   <div>
+     <router-link to="/materiels"><img alt="material" src="../../assets/materials.png"/></router-link>
+               <p> Materiels</p>
+   </div>
+
+      <div>
+        <router-link to="/etudiants"> <img alt="students" src="../../assets/students.png"/></router-link>
+           <p> Etudiants</p>
+      </div>
+
+    <div>
+       <img alt="statistic" src="../../assets/statistic.png"/>
+     <p> Statistique</p>
+    </div>
     </div>
   </div>
   
