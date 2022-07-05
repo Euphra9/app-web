98d8c7f39f96d87f335ed2fbd336a5bed0515d4f
Luzolo Euphraim <euphraluzolo@gmail.com>
2022-07-05 22:52:36 +0200

    update material en cours

--- src/components/Main/Materials/UpdateMaterial.vue
+++ src/components/Main/Materials/UpdateMaterial.vue
@@ -33,15 +33,15 @@
   <div>
       <label for="dateAchat" >Date achat </label> <br/>
       <input type="date" id="dateAchat" name="dateAchat"
-       min="2000-01-01" max="3000-01-01" >
+       min="2000-01-01" max="3000-01-01" v-model="supply.DateAchat">
    </div>
    <div>
       <label for="prixAchat" >Prix Achat </label> <br/>
-      <input type="number" id="prixAchat" name="prixAchat" min="0" >
+      <input type="number" id="prixAchat" name="prixAchat" min="0" v-model="supply.PrixAchat" >
    </div>
      <div>
       <label for="fournisseur" >Fournisseur </label> <br/>
-      <select id="fournisseur" name="type" v-model="material.Fournisseur">
+      <select id="fournisseur" name="type" v-model="supplierId">
       <option></option>
          <option  v-for="supplier in suppliers" :key="supplier.NumFournisseur"> 
          {{supplier.NumFournisseur}} - {{supplier.Nom}}
@@ -64,13 +64,17 @@
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
+
 
 export default{
   data(){
     return {
       id:this.$route.params.id,
       material:{},
+      supply:{},
       supplier:{},
+      supplierId:"",
       name:"",
       suppliers:{}
 
@@ -84,6 +88,21 @@ export default{
     .then(response => {
       this.material=response.data
       this.name=this.material.Nom;
+
+       axios.get('http://localhost:8081/api/supply/'+this.material.CodeBarre)
+      .then(response => {
+        this.supply=response.data
+        this.supply.DateAchat=moment( this.supply.DateAchat).format('YYYY-MM-DD')
+
+          axios.get('http://localhost:8081/api/supply/supplier/'+this.supply.NumFournisseur)
+          .then(response => {
+            this.supplier=response.data
+            this.supplierId=this.supplier.NumFournisseur+" - "+this.supplier.Nom;
+            }) // creation de la promesse
+          .catch()
+
+        }) // creation de la promesse
+     .catch()
         
 
       }) // creation de la promesse
